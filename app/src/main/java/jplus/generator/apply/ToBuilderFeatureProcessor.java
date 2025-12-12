073cd2def177b895c1e0c5eedca2e5e736cbdce6
Cheol Jeon <nieuwmijnleven@outlook.com>
2025-12-12 13:57:07 +0100

    feature: add the support for @Nullable annotation

---
+++ app/src/main/java/jplus/generator/apply/ToBuilderFeatureProcessor.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2025 Cheol Jeon <nieuwmijnleven@outlook.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jplus.generator.apply;
+
+import jplus.base.Modifier;
+import jplus.base.SymbolInfo;
+import jplus.base.TypeInfo;
+import jplus.util.CodeGenUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ToBuilderFeatureProcessor implements ApplyFeatureProcessor {
+    @Override
+    public void process(ApplyFeatureProcessingContext context) {
+        if (context.hasProcessed("tobuilder")) return;
+
+        String methodName = "^toBuilder$_";
+        if (context.getClassSymbolTable().contains(methodName, TypeInfo.Type.Method)) {
+            return;
+        }
+
+        if (!context.getClassSymbolTable().contains("Builder", TypeInfo.Type.Class) || !context.hasProcessed("builder")) {
+            new BuilderFeatureProcessor().process(context);
+        }
+
+        String indentation = context.getIndentation();
+        String doubleIndentation = indentation.repeat(2);
+
+        TypeInfo targetClassTypeInfo = context.getTargetClassTypeInfo();
+        System.err.println("[ToBuilderFeatureProcessor] targetClassTypeInfo = " + targetClassTypeInfo);
+        String typeArgumentString = CodeGenUtils.getGenericTypeArgumentString(targetClassTypeInfo);
+        System.err.println("[ToBuilderFeatureProcessor] typeArgumentString = " + typeArgumentString);
+
+
+        String builderTypeName = "Builder" + typeArgumentString;
+        String builder = "\npublic " + builderTypeName + " toBuilder() {\n";
+        builder += indentation + "return new Builder" + typeArgumentString + "()";
+
+        for (String fieldName : context.getFieldList()) {
+            SymbolInfo symbolInfo = context.getClassSymbolTable().resolve(fieldName);
+
+            List<Modifier> excludedModifiers = List.of(Modifier.STATIC);
+            if (symbolInfo.getModifierList().stream().anyMatch(modifier -> excludedModifiers.contains(modifier))) {
+                continue;
+            }
+
+            builder += "\n" + doubleIndentation + "." + fieldName + "(this." + fieldName + ")";
+        }
+
+        builder += ";\n}\n";
+
+        context.appendMethodPartText(builder);
+//        ApplyFeatureProcessor.super.process(context);
+        context.addProcessedAction("tobuilder");
+    }
+}
