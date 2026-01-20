7f675377590275810a1f82de24bcfdcd8142bb0f
Cheol Jeon <nieuwmijnleven@outlook.com>
2026-01-20 16:43:44 +0100

    feature: add the support for @Nullable annotation

--- app/src/main/java/jplus/analyzer/nullability/InvocationDeclarationContext.java
+++ app/src/main/java/jplus/analyzer/nullability/InvocationDeclarationContext.java
@@ -1,12 +1,16 @@
 package jplus.analyzer.nullability;
 
 import jplus.base.JPlus25Parser;
+import jplus.base.Modifier;
 import jplus.util.Utils;
 import org.antlr.v4.runtime.ParserRuleContext;
+
+import java.util.EnumSet;
 
 public interface InvocationDeclarationContext {
     ParserRuleContext originalContext();
     JPlus25Parser.FormalParameterListContext formalParameterList();
+    EnumSet<Modifier> modifiers();
     String methodName();
 
     static InvocationDeclarationContext from(JPlus25Parser.MethodDeclarationContext ctx) {
@@ -19,6 +23,15 @@ public interface InvocationDeclarationContext {
             @Override
             public JPlus25Parser.FormalParameterListContext formalParameterList() {
                 return ctx.methodHeader().methodDeclarator().formalParameterList();
+            }
+
+            @Override
+            public EnumSet<Modifier> modifiers() {
+                EnumSet<Modifier> modifierEnumSet = EnumSet.noneOf(Modifier.class);
+                for (JPlus25Parser.MethodModifierContext methodModifierContext : ctx.methodModifier()) {
+                    modifierEnumSet.add(Modifier.valueOf(methodModifierContext.toString().toUpperCase()));
+                }
+                return modifierEnumSet;
             }
 
             @Override
@@ -38,6 +51,15 @@ public interface InvocationDeclarationContext {
             @Override
             public JPlus25Parser.FormalParameterListContext formalParameterList() {
                 return ctx.constructorDeclarator().formalParameterList();
+            }
+
+            @Override
+            public EnumSet<Modifier> modifiers() {
+                EnumSet<Modifier> modifierEnumSet = EnumSet.noneOf(Modifier.class);
+                for (JPlus25Parser.ConstructorModifierContext constructorModifierContext: ctx.constructorModifier()) {
+                    modifierEnumSet.add(Modifier.valueOf(constructorModifierContext.toString().toUpperCase()));
+                }
+                return modifierEnumSet;
             }
 
             @Override
