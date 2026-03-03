80a11e3232e4710d4aa55256a72105427581699e
Cheol Jeon <nieuwmijnleven@outlook.com>
2026-03-03 10:54:07 +0100

    feat: update FragmentedTextTest with improved test cases and move to editor package

--- app/src/test/java/jplus/editor/FragmentedTextTest.java
+++ app/src/test/java/jplus/editor/FragmentedTextTest.java
@@ -26,9 +26,9 @@
 
 package jplus.editor;
 
-import jplus.generator.TextChangeRange;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
@@ -70,8 +70,8 @@ class FragmentedTextTest {
         TextChangeRange targetRange = new TextChangeRange(1, 0, 1, projected.length() - 1);
         String projectedText = ft.projectOn(targetRange, projected);
 
-        assertTrue(Objects.equals(ft.toString(), "Hello"));
-        assertTrue(Objects.equals(projectedText, "Hello World"));
+        assertEquals("Hello", ft.toString());
+        assertEquals("Hello World", projectedText);
     }
 
     @Test
@@ -137,5 +137,76 @@ class FragmentedTextTest {
         FragmentedText ft = new FragmentedText(text);
 
         assertEquals(text, ft.toString());
+    }
+
+    @Test
+    void testUpdateSingleLine() {
+        TextChangeRange range = new TextChangeRange(12, 10, 12, 29);
+        String original = "ive either way rebel";
+        FragmentedText fragmentedText = new FragmentedText(range, original);
+
+        fragmentedText.update(new TextChangeRange(12,10,12, 12), "meiden");
+        assertEquals("meiden either way rebel", fragmentedText.toString());
+
+        fragmentedText.update(new TextChangeRange(12,10,12, 12), "meisje");
+        assertEquals("meisje either way rebel", fragmentedText.toString());
+
+        fragmentedText.update(new TextChangeRange(12,25,12, 29), "verzet");
+        assertEquals("meisje either way verzet", fragmentedText.toString());
+
+        fragmentedText.update(new TextChangeRange(12,25,12, 29), "rebel");
+        assertEquals("meisje either way rebel", fragmentedText.toString());
+
+        fragmentedText.update(new TextChangeRange(12,14,12, 23), "off the record");
+        assertEquals("meisje off the record rebel", fragmentedText.toString());
+
+        fragmentedText.update(new TextChangeRange(12,14,12, 23), "either way");
+        assertEquals("meisje either way rebel", fragmentedText.toString());
+
+    }
+
+    @Test
+    void testUpdateMultiLine() throws IOException {
+        //String text = Files.readString(Paths.get("./src/test/samples/ElvisOperator.jplus"));
+        //TextChangeRange textRange = new TextChangeRange(1,0, 9,0);
+        TextChangeRange textRange = new TextChangeRange(4, 25, 9, 4);
+        String text = "{\n" +
+                "        String? s1 = null;\n" +
+                "        String s2 = s1 ?: \"jplus\";\n" +
+                "        System.out.printf(\"s1 = %s\\n\", s1 ?: \"null-value\");\n" +
+                "        System.out.printf(\"s2 = %s\\n\", s2);\n" +
+                "    }";
+        FragmentedText fragmentedText = new FragmentedText(textRange, text);
+        assertEquals(text, fragmentedText.toString());
+
+        TextChangeRange range = new TextChangeRange(4, 25, 9, 4);
+        String replaced = "{\n" +
+                "        String? s1 = null;\n" +
+                "        String s2 = (((s1)!=null)?(s1):(\"jplus\"));\n" +
+                "        System.out.printf(\"s1 = %s\\n\", (((s1)!=null)?(s1):(\"null-value\")));\n" +
+                "        System.out.printf(\"s2 = %s\\n\", s2);\n" +
+                "    }";
+        fragmentedText.update(range, replaced);
+        //System.out.println(stringVersion.toString());
+        assertEquals(replaced, fragmentedText.toString());
+    }
+
+    @Test
+    void testUpdateMultiLine2() throws IOException {
+        //String text = Files.readString(Paths.get("./src/test/samples/ElvisOperator.jplus"));
+        //TextChangeRange textRange = new TextChangeRange(1,0, 9,0);
+        TextChangeRange textRange = new TextChangeRange(6, 0, 9, 0);
+        String text = "apply {\n" +
+                "    User: data, builder;\n" +
+                "    User.Profile: data, constructors(all);\n" +
+                "}";
+        FragmentedText fragmentedText = new FragmentedText(textRange, text);
+        assertEquals(text, fragmentedText.toString());
+
+        TextChangeRange range = new TextChangeRange(6, 0, 9, 0);
+        String replaced = "";
+        fragmentedText.update(range, replaced);
+        //System.out.println(stringVersion.toString());
+        assertEquals(replaced, fragmentedText.toString());
     }
 }
