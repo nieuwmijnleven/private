2dcd013ad861daaf0663a98198c3e55d066f9ab4
Cheol Jeon <nieuwmijnleven@outlook.com>
2026-03-06 16:06:11 +0100

    feat: intellij-plugin enhancement

--- intellij-plugin/src/main/java/jplus/plugin/intellij/psi/NormalClassDeclarationPsiElement.java
+++ intellij-plugin/src/main/java/jplus/plugin/intellij/psi/NormalClassDeclarationPsiElement.java
@@ -27,21 +27,68 @@
 package jplus.plugin.intellij.psi;
 
 import com.intellij.lang.ASTNode;
+import com.intellij.openapi.util.NlsSafe;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiNameIdentifierOwner;
+import com.intellij.psi.PsiNamedElement;
 import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.IncorrectOperationException;
 import jplus.base.JADEx25Parser;
+import jplus.plugin.intellij.JPlusLanguage;
 import jplus.plugin.intellij.JPlusTokenTypes;
+import org.antlr.intellij.adaptor.lexer.RuleIElementType;
 import org.antlr.intellij.adaptor.psi.ANTLRPsiNode;
+import org.antlr.intellij.adaptor.psi.ScopeNode;
+import org.antlr.intellij.adaptor.psi.Trees;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class NormalClassDeclarationPsiElement extends ANTLRPsiNode {
+public class NormalClassDeclarationPsiElement extends ANTLRPsiNode implements PsiNameIdentifierOwner, ScopeNode {
+
     public NormalClassDeclarationPsiElement(@NotNull ASTNode node) {
         super(node);
     }
 
     @Override
-    public String getName() {
+    public @Nullable PsiElement getNameIdentifier() {
+        IElementType typeIdentifier = getTypeIdentifierIElementType();
+
+        PsiElement composite = findChildByType(typeIdentifier);
+        if (composite == null) return null;
+
+        return PsiTreeUtil.getDeepestFirst(composite);
+    }
+
+    private IElementType getTypeIdentifierIElementType() {
         IElementType typeIndentifier = JPlusTokenTypes.getRuleElementType(JADEx25Parser.RULE_typeIdentifier);
-        ASTNode child = getNode().findChildByType(typeIndentifier);
-        return child != null ? child.getText() : super.getName();
+        return typeIndentifier;
+    }
+
+    @Override
+    public String getName() {
+        PsiElement id = getNameIdentifier();
+        return id != null ? id.getText() : super.getName();
+    }
+
+//    @Override
+//    public String getName() {
+//        IElementType typeIndentifier = JPlusTokenTypes.getRuleElementType(JADEx25Parser.RULE_typeIdentifier);
+//        ASTNode child = getNode().findChildByType(typeIndentifier);
+//        return child != null ? child.getText() : super.getName();
+//    }
+
+    @Override
+    public PsiElement setName(@NlsSafe @NotNull String newName) throws IncorrectOperationException {
+
+        PsiElement newNode = Trees.createLeafFromText(getProject(), JPlusLanguage.INSTANCE, getContext(), newName, getTypeIdentifierIElementType());
+
+        PsiElement id = getNameIdentifier();
+        return id.replace(newNode);
+    }
+
+    @Override
+    public @Nullable PsiElement resolve(PsiNamedElement element) {
+        return null;
     }
 }
